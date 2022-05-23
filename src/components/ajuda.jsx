51a6c4f8e4685cdef2223266bd7114b8f8937a80
purplebridgediscovery <purplebridgediscovery@gmail.com>
2022-05-22 21:58:36 -0300

    Updates

--- src/components/ajuda.jsx
+++
@@ -1,6 +0,0 @@
-import "./ajuda.css";
-export function Ajuda(){
-    return(
-        <a id="ajuda" href={"https://api.whatsapp.com/send?phone=5561995037493&text="+ "Venho do site preciso de ajuda/tirar uma duvida"} target="_blank"/>
-    )
-}
